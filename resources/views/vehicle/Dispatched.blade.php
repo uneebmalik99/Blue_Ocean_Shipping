55ae99d22c6acf8aea65069e91ef5836c2df0b6a
QaziKashif <kashifqazi745@gmail.com>
2022-12-27 12:10:25 +0500

    dispatch delete vehicle

--- resources/views/vehicle/Dispatched.blade.php
+++ resources/views/vehicle/Dispatched.blade.php
@@ -68,7 +68,7 @@
                         </a>
                     </button>
                     <button class="delete-button">
-                        <a href={{ url(@$module['action'] . '/delete/' . @$val[@$module['db_key']]) }}>
+                        <a href={{route('vehicle.delete', @$val['id'])}}>
                             <svg width="14" height="13" viewBox="0 0 12 12" fill="none"
                                 xmlns="http://www.w3.org/2000/svg">
                                 <path
