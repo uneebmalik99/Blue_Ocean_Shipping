7d30e4fb2cf26df83d0d7c6a4eb1e7fb2c20e81b
TahirIsmail <vjtary104@gmail.com>
2023-01-23 23:00:36 +0500

    Push login logout code

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,17 +8,35 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Spatie\Permission\Traits\HasRoles;
 use App\Traits\LockableTrait;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
-    use HasFactory,HasRoles,LockableTrait,Notifiable;
+    use HasFactory,HasRoles,LockableTrait,Notifiable,HasApiTokens;
     // use HasRoles;
     // use LockableTrait;
     // use Notifiable;
     // use softDeletes;
     protected $primaryKey = 'id';
-    protected $table = "user";
+    protected $table = "users";
     protected $guarded = [];
-    
+      /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];    
 
     public function vehicles()
     {
