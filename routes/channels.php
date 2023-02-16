55a80705faf32ed96601d4a6848be2e476786967
TahirIsmail <vjtary104@gmail.com>
2023-02-16 15:57:18 +0500

    migraitner

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
-
+use App\Models\Notifications;
 /*
 |--------------------------------------------------------------------------
 | Broadcast Channels
@@ -15,4 +15,8 @@ use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
+});
+
+Broadcast::channel('user.{userId}', function ($user, $userId) {
+    return true;
 });
